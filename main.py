89bffb941ecc1cbda5a5ba8f109a9679714aafe6
Payton Knezek <prknezek@gmail.com>
2022-10-23 13:55:18 -0500

    Main Bot Functionality

---
+++ main.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+
+TOKEN = 'MTAzMzgwNjMzNDE1NzY1NjE2NA.GM3woV.urP4KY5FCmKu0O3clSxubmxi4jnFP_sxTthTfo'
+intents = discord.Intents.all() # make sure commands work
+# initialize our bot with command prefix '!'
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+# when the bot is ready to start receiveing commands it will execute this function
+async def on_ready():
+    print("Jerma is ready for battle")
+    print("-------------------------")
+
+@bot.command()
+# name of function is what the user will type to call this command
+async def hello(ctx):
+    await ctx.send("Hello")
+
+# run the bot after initializing all commands
+bot.run(TOKEN)
