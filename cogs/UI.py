2f65501a66f59c86e04ba497b4ef090e15e7f1c8
Payton Knezek <prknezek@gmail.com>
2022-11-02 00:34:46 -0500

    dropdown menu commands

--- cogs/UI.py
+++ cogs/UI.py
@@ -23,12 +23,42 @@ class Buttons(nextcord.ui.View) :
         self.value = False # allows us to determine if button is clicked
         self.stop()
 
+# class to make the dropdown
+class Dropdown(nextcord.ui.Select) :
+    def __init__(self) :
+        select_options = [
+            nextcord.SelectOption(label="Item 1", description="Item 1 in dropdown"),
+            nextcord.SelectOption(label="Item 2", description="Item 2 in dropdown"),
+            nextcord.SelectOption(label="Item 3", description="Item 3 in dropdown")
+        ]
+        super().__init__(placeholder="Dropdown Options:", min_values=1, max_values=1, options=select_options)
+        
+    async def callback(self, interaction : Interaction) :
+        # user selected options are stored in values
+        if self.values[0] == 'Item 1' :
+            return await interaction.response.send_message("Clicked Item 1")
+        elif self.values[0] == 'Item 2' :
+            return await interaction.send("Clicked Item 2")
+        else :
+            return await interaction.send("Clicked Item 3")
+
+# class to render the dropdown
+class DropdownView(nextcord.ui.View) :
+    def __init__(self) :
+        super().__init__()
+        self.add_item(Dropdown())
+
 class UI(commands.Cog) :
     def __init__(self, bot : commands.Bot) :
         self.bot = bot
 
     serverId = 1033811091828002817
 
+    @commands.Cog.listener()
+    async def on_ready(self) :
+        print("UI Cog Loaded")
+
+    # function name cannot be same name as a button
     @nextcord.slash_command(name = "button", description="Button", guild_ids=[serverId])
     async def testing_buttons(self, interaction : Interaction) :
         view = Buttons()
@@ -41,7 +71,11 @@ class UI(commands.Cog) :
             print ("You clicked the button")
         else :
             print ("idk")
-
-
+    
+    @nextcord.slash_command(name="dropdown", description="creates a dropdown", guild_ids=[serverId])
+    async def drop(self, interaction : Interaction) :
+        view = DropdownView()
+        await interaction.send("Here's a dropdown", view=view)
+    
 def setup(bot) :
     bot.add_cog(UI(bot))
