1d548f8969bc35e51c11f81d54c6f5cb3e4be8b5
Hemanth <hmukesh@outlook.com>
2022-11-12 15:38:41 -0600

    formatting
    added comments etc

--- cogs/UI.py
+++ cogs/UI.py
@@ -4,7 +4,9 @@ import nextcord
 from nextcord import Interaction, SlashOption
 from nextcord.ext import commands
 
-# load names for birthday messsage
+# ---------------------------------------------------------------------------- #
+#                     load all names for birthday messsage                     #
+# ---------------------------------------------------------------------------- #
 bdaynames = []
 f = open("Jerma985Bot/cogs/resources/birthdaynames.txt")
 for name in f.readlines():
@@ -12,22 +14,28 @@ for name in f.readlines():
 bdaynames[len(bdaynames)-1] = "zoe"
 f.close()
 
-# load wiki pages
+# ---------------------------------------------------------------------------- #
+#                                load wiki pages                               #
+# ---------------------------------------------------------------------------- #
 wikisites = []
 f = open("Jerma985Bot/cogs/resources/wikisites.txt")
 for site in f.readlines():
     wikisites.append(str(site).strip())
 f.close()
 
+# ---------------------------------------------------------------------------- #
+#                         class for birthday selection                         #
+# ---------------------------------------------------------------------------- #
 class BdayDropdown(nextcord.ui.Select) :
 
     def __init__(self, name: str) :
+        # create the two options with the given name and style stored in value
         select_options = [ nextcord.SelectOption(label=name[0].upper()+name[1:]+" Style 1", value=name + "1", description="Birthday Video Style 1"),
                            nextcord.SelectOption(label=name[0].upper()+name[1:]+" Style 2", value=name + "2", description="Birthday Video Style 2") ]
         super().__init__(placeholder="Video Style:", min_values=1, max_values=1, options=select_options)
 
     async def callback(self, interaction : Interaction) :
-        # user selected options are stored in values
+        # name and style are stored in value
         print(self.values[0])
 
         style = self.values[0][-1:]
@@ -35,11 +43,18 @@ class BdayDropdown(nextcord.ui.Select) :
 
         return await interaction.send("https://cameoapi.jerma.io/?type=birthday_type0{}&name1={}".format(style, name))
 
+# ---------------------------------------------------------------------------- #
+#                       class for birthday selection view                      #
+# ---------------------------------------------------------------------------- #
 class BdayDropdownView(nextcord.ui.View) :
     def __init__(self, name:str) :
         super().__init__()
         self.add_item(BdayDropdown(name=name))
 
+
+# ---------------------------------------------------------------------------- #
+#                                  main class                                  #
+# ---------------------------------------------------------------------------- #
 class UI(commands.Cog) :
     def __init__(self, bot : commands.Bot) :
         self.bot = bot
@@ -50,6 +65,7 @@ class UI(commands.Cog) :
     async def on_ready(self) :
         print("UI Cog Loaded")
 
+    # ---------------------------- send birthday names --------------------------- #
     @nextcord.slash_command(name="birthday-supported-names", description="DM's a list of supported birthday names", guild_ids=[serverId])
     async def birthdayNamesDM(self, interaction : Interaction):
         allnames = ""
@@ -59,6 +75,7 @@ class UI(commands.Cog) :
         await interaction.user.send(embed=embed)
         await interaction.send("Sent! Check your DMs.", ephemeral=True)
 
+    # ----------------------------- birthday message ----------------------------- #
     @nextcord.slash_command(name="birthday-message", description="sends a personalized jerma birthday message", guild_ids=[serverId])
     async def birthdayMsg(self, interaction : Interaction, name: str = SlashOption(name="name", description="For list of supported names, type /birthday-supported-names")) :
         name = name.lower()
@@ -67,13 +84,14 @@ class UI(commands.Cog) :
             return
         
         view = BdayDropdownView(name=name)
-        # interaction.daname = name
         await interaction.send("Choose a birthday message style for "+name[0].upper()+name[1:], view=view)
 
+    # --------------------------------- wiki link -------------------------------- #
     @nextcord.slash_command(name = "wiki", description="Link to Jerma wiki", guild_ids=[serverId])
     async def wiki_link(self, interaction : Interaction) :
         await interaction.response.send_message("https://jerma-lore.fandom.com/wiki/Jerma985")
 
+    # ----------------------------- random wiki link ----------------------------- #
     @nextcord.slash_command(name = "rwikipage", description="Link to a random Jerma wikipage", guild_ids=[serverId])
     async def wiki_link(self, interaction : Interaction) :
         numsites = len(wikisites)
