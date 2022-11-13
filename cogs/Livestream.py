be32c42d3cf4d633703e4af698ee689be6add8f7
Hemanth <hmukesh@outlook.com>
2022-11-13 16:57:23 -0600

    finished slots game
    finished slots game

--- cogs/Livestream.py
+++ cogs/Livestream.py
@@ -1,12 +1,22 @@
+import asyncio
 from random import randint
 
 import nextcord
 import requests
-from nextcord import Interaction
+from nextcord import Interaction, SlashOption
 from nextcord.ext import commands, tasks
 from twitchAPI.twitch import Twitch
+from PIL import Image
+
+import cogs.Data as database
+import cogs.BlackJack as blackjack
 
 from apikeys import *
+
+MR_GREEN_URL = "https://static.wikia.nocookie.net/jerma-lore/images/2/25/MrGreen_RosterFace.png/revision/latest/top-crop/width/360/height/360?cb=20210426041715"
+
+IMG_WIDTH = 470
+IMG_HEIGHT = 60
 
 # authentication w/ Twitch API
 client_id = TWITCH_CLIENT_ID
@@ -29,7 +39,7 @@ API_HEADERS = {
     'Authorization': 'Bearer ' + keys['access_token']
 }
 
-# ---------------------------------- Emote Request ---------------------------------- #
+# ------------------------------- Emote Request ------------------------------ #
 CHANNEL_ID = 23936415
 emotereq = requests.get(f"https://api.twitch.tv/helix/chat/emotes?broadcaster_id={CHANNEL_ID}", headers=API_HEADERS)
 emotejson = emotereq.json()['data']
@@ -48,6 +58,7 @@ for img in emote_images_url :
     print(img)
 
 print(f"-------------- Num of emotes: {len(emote_images_url)} --------------")
+
 
 # returns true if streamer is online and false if not online
 def check_user(user):
@@ -130,9 +141,9 @@ class Livestream(commands.Cog) :
         # starts the loop to scan for streaming activity
         live_notifs_loop.start()
 
-    @nextcord.slash_command(name="rstream", description="retrieves a random Jerma985 stream", guild_ids=[serverId])
     # gives user a random broadcast from the past 60 days
     # twitch only stores broadcasts for 60 days
+    @nextcord.slash_command(name="rstream", description="retrieves a random Jerma985 stream", guild_ids=[serverId])    
     async def rstream(self, interaction : Interaction):
         # gets broadcaster info
         broadcaster_info = twitch.get_users(user_ids=None, logins="jerma985")["data"][0]
@@ -182,11 +193,94 @@ class Livestream(commands.Cog) :
         embed.add_field(name="Published:", value=published, inline=True)
         embed.set_footer(text=f"Sent by: {user}")
         await interaction.send(embed=embed)
-
+    
     @nextcord.slash_command(name="twitch", description="Sends a link to Jerma985's twitch", guild_ids=[serverId])
     async def twitch(self, interaction : Interaction) :
         await interaction.send("https://twitch.tv/jerma985")
+    
+    @nextcord.slash_command(name="slots", description="A slots game", guild_ids=[serverId])
+    async def slots_game(self, interaction : Interaction, starting_bet:float = SlashOption(name="bet", description="Amount of money to bet")) :
         
+        # ---------------------- check if starting_bet is valid ---------------------- #
+        starting_bet = database.normal_round(starting_bet, 2)        
+        if starting_bet <= 0.00:
+            return await interaction.send("What, you think I'm an idiot?! - Mr. Green", ephemeral=True)
+
+        # ---------------- check balance and take money away from user --------------- #
+        try:
+            user_balance_raw = database.retrieveData(interaction.guild.id, interaction.user, ['MONEY'])[0]
+        except:
+            return await interaction.send("Uh oh! I couldn't connect to the database.")
+        if user_balance_raw[0] == '$':
+            user_balance = float(user_balance_raw[1:])
+        else:
+            user_balance = float(user_balance_raw)
+        if user_balance < starting_bet:
+            return await interaction.send("You're too broke to play! - Mr. Green", ephemeral=True)
+        user_balance -= starting_bet
+        database.storeData(interaction.guild.id, interaction.user, {'MONEY': str(user_balance)})
+        
+
+        felt_img = Image.open("./cogs/resources/transparentbg.png")
+        felt_edit = Image.new("RGBA", (IMG_WIDTH, IMG_HEIGHT))
+        felt_edit.paste(felt_img)
+        felt_file = blackjack.convert_to_file(felt_edit)
+
+        # ----------------------------- send instructions ---------------------------- #
+        embed = nextcord.Embed(title="Slots", color=0x508f4a)
+        embed.set_author(name= "Mr. Green's Casino", icon_url=MR_GREEN_URL)
+        embed.add_field(name="If the pictures line up, you could win big! Good luck!\n-----------------------------------------------------------", 
+                        value="**${:.2f}** has been **withdrawn** from {}'s account!".format(starting_bet, str(interaction.user)), inline=False)
+        embed.add_field(name="Slot 1", value="rolling...", inline=True)
+        embed.add_field(name="Slot 2", value="rolling...", inline=True)
+        embed.add_field(name="Slot 3", value="rolling...", inline=True)
+        embed.set_image(url="attachment://table.png")
+        message = await interaction.send(embed=embed, file=felt_file)
+
+        imgIndexList = []
+        xcords = [0, 150, 316]
+        for fieldIndex in range(1,len(embed.fields)):
+
+            await asyncio.sleep(fieldIndex/2+0.5)
+            imgIndex = randint(0,4)
+            imgIndexList.append(imgIndex)
+            embed.remove_field(fieldIndex)
+            embed.insert_field_at(fieldIndex, name=f"Slot {fieldIndex}", value=f"{imgIndex}", inline=True)            
+            
+            #do some more file shit 
+            coordinates = (xcords[fieldIndex-1], 0)
+            print(coordinates)
+
+            felt_img = Image.open("./cogs/resources/transparentbg.png")
+            #felt_edit = Image.new("RGBA", (IMG_WIDTH, IMG_HEIGHT))
+
+            slot_img = Image.open(requests.get(emote_images_url[imgIndex], stream=True).raw)
+            felt_edit.paste(slot_img, coordinates)
+            felt_file = blackjack.convert_to_file(felt_edit)
+
+            await message.edit(embed=embed, file=felt_file)
+
+        await asyncio.sleep(0.7)
+
+        # calculate payout
+        payout = 0
+        multiplier = 1
+        if imgIndexList[0] == imgIndexList[1]:
+            payout += starting_bet
+            multiplier += imgIndexList[0]+3.5
+        if imgIndexList[1] == imgIndexList[2]:
+            payout += starting_bet
+            multiplier += imgIndexList[1]+3.5
+        payout *= multiplier
+
+        # payout
+        user_balance += payout
+        database.storeData(interaction.guild.id, interaction.user, {'MONEY': str(user_balance)})
+        embed = nextcord.Embed(title="Payment", color=0x508f4a, description="**${:.2f}** has been **deposited** to {}'s account!\nTotal Balance: **${:.2f}**".format(payout, str(interaction.user), user_balance))
+        embed.set_author(name= "Mr. Green's Casino", icon_url=MR_GREEN_URL)
+        return await interaction.send(embed=embed)
+
+
 # export cog to bot
 def setup(bot: commands.Bot) :
     bot.add_cog(Livestream(bot))
